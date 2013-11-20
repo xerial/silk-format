3d8304b3bf769813d3539663d3bbcb6d3e55f830
Taro L. Saito <leo@xerial.org>
2013-11-20 10:54:10 +0900

    initial commit

---
+++ src/main/scala/xerial/silk/text/SilkGrammar.scala
@@ -0,0 +1,30 @@
+//--------------------------------------
+//
+// SilkGrammar.scala
+// Since: 2013/11/20 10:42 AM
+//
+//--------------------------------------
+
+package xerial.silk.text
+
+import xerial.core.log.Logger
+
+
+object SilkGrammar extends Grammar with Logger {
+  import Token._
+
+  // Silk grammar rules
+  "expr" := String | LParen ~ "expr" ~ RParen
+  "silk" := DataLine | "node" | "preamble" | LineComment | BlankLine
+  "preamble" := Preamble ~ QName ~ option(Name) ~ option("preambleParams")
+  "preambleParams" := (Separator ~ repeat("preambleParam", Comma)) | (LParen ~ repeat("preambleParam", Comma) ~ RParen)
+  "preambleParam" := Name ~ option(Colon ~ "preambleParamValue")
+  "preambleParamValue" := "value" | "typeName"
+  "typeName" := QName ~ option(LSquare ~ oneOrMore(QName, Comma) ~ RSquare)
+  "node" := option(Indent) ~ Name ~ option("nodeParams") ~ option("nodeParamSugar" | "nodeParams")
+  "nodeParamSugar" := Separator ~ repeat("param", Comma)
+  "nodeParams" := LParen ~ repeat("param", Comma) ~ RParen ~ option(Colon ~ NodeValue)
+  "param" := Name ~ option(Colon ~ "value")
+  "value" := NodeValue | Name | QName | Token.String | Integer | Real | "tuple"
+  "tuple" := LParen ~ repeat("value", Comma) ~ RParen
+}
